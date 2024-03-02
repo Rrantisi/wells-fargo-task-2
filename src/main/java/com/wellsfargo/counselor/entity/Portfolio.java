808f6eabb0de03640e77a7378d238f5d203c5484
Rrantisi <rantisireham19@gmail.com>
2024-03-02 10:03:02 -0500

    fixes missing code in remote repo

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -26,4 +26,33 @@ public class Portfolio {
 	@Column(nullable = false)
 	private Date creationDate;
 	
-	pr
+	protected Portfolio() {
+		
+	}
+	
+	public Portfolio(Client client, Date creationDate) {
+		this.client = client;
+		this.creationDate = creationDate;
+	}
+	
+	public Long getPortfolioId() {
+		return portfolioId;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+}
